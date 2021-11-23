816ae3a79d27776ad04cb20320ecbd035b46dc4d
walkeralexander <34752031+walkeralexander@users.noreply.github.com>
2021-11-22 23:47:52 -0500

    Entirely formatting. Look this over later.

--- pyGEDI/get.py
+++ pyGEDI/get.py
@@ -4,33 +4,35 @@ import h5py
 
 
 def getH5(fileh5):
-	return h5py.File(fileh5, 'r')
+    return h5py.File(fileh5, 'r')
+
 
 def getLayer(layer, files):
-    dictionary={}
-    for fileh5 in files:      
-        lista=h5layers(fileh5['BEAM0000'])        
-        lista=str(lista).replace('[', '').replace(']', '').replace('/BEAM0000/', '')
-        lista=ast.literal_eval(lista)       
-        r=re.compile('.*'+layer)
-        mylistafilter=list(filter(r.match,lista))        
-        if len(mylistafilter)>0:
-            dictionary[fileh5.filename]=mylistafilter            
+    dictionary = {}
+    for fileh5 in files:
+        lista = h5layers(fileh5['BEAM0000'])
+        lista = str(lista).replace('[', '').replace(
+            ']', '').replace('/BEAM0000/', '')
+        lista = ast.literal_eval(lista)
+        r = re.compile('.*'+layer)
+        mylistafilter = list(filter(r.match, lista))
+        if len(mylistafilter) > 0:
+            dictionary[fileh5.filename] = mylistafilter
     return dictionary
 
-def getBeam(shot_number,file):
-    for beam in ['BEAM0000','BEAM0001','BEAM0010','BEAM0011','BEAM0101','BEAM0110','BEAM1000','BEAM1011']: 
+
+def getBeam(shot_number, file):
+    for beam in ['BEAM0000', 'BEAM0001', 'BEAM0010', 'BEAM0011', 'BEAM0101', 'BEAM0110', 'BEAM1000', 'BEAM1011']:
         if shot_number in file[beam]['shot_number'][:]:
             break
     return beam
 
 
-def h5layers(arg):  
-    lista=[]
+def h5layers(arg):
+    lista = []
     if isinstance(arg, h5py.Dataset):
         return arg.name
     if isinstance(arg, h5py.Group):
         for i in arg.keys():
             lista.append(h5layers(arg[i]))
     return lista
-
